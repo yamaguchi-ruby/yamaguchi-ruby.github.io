f1b0b2abbb420441a50fa56fc3ca950e94c1e6f0
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2020-07-31 09:06:13 +0900

    訂正

--- assets/js/public.js
+++ assets/js/public.js
@@ -8,14 +8,26 @@ function menu(){
             nth = 3
         else if(type == "news")
             nth = 2
-        else if(type == "sars2")
-            nth = 6
         
         e.innerHTML = `\
         body header nav ul li:nth-child(${nth}) a{
             border-bottom: 2px solid var(--dark);
         }`
         document.head.append(e)
+        
+    }else{
+        m = location.pathname.match(/^\/(.*?)$/)
+        if(m){
+            let type = m[1]
+            let nth
+            if(type == "sars2")
+                nth = 6
+            e.innerHTML = `\
+            body header nav ul li:nth-child(${nth}) a{
+                border-bottom: 2px solid var(--dark);
+            }`
+            document.head.append(e)
+        }
     }
 }
 
